1d0dffc543e8c26d9c9148ebb88a9edb39e55621
SelineXD <seline.and2@gmail.com>
2017-10-04 16:08:47 +0200

    SEL

---
+++ registrate.php
@@ -0,0 +1,99 @@
+<?php 
+session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=searchlove', 'root', '');
+?>
+<!DOCTYPE html> 
+<html> 
+<head>
+        <title>searchlove</title>
+		<link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+	<div id="header">
+	<img src="logo.png" width="200px">
+		<br/><strong>das Datingportal</strong>
+	</div>	
+	<div id="content">
+ 
+<?php
+$showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
+ 
+if(isset($_GET['register'])) {
+ $error = false;
+ $vorname = $_POST['vorname'];
+ $nachname = $_POST['nachname'];
+ $email = $_POST['email'];
+ $passwort = $_POST['passwort'];
+ $passwort2 = $_POST['passwort2'];
+  
+ if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+ $error = true;
+ } 
+ if(strlen($passwort) == 0) {
+ echo 'Bitte ein Passwort angeben<br>';
+ $error = true;
+ }
+ if($passwort != $passwort2) {
+ echo 'Die Passwörter müssen übereinstimmen<br>';
+ $error = true;
+ }
+ 
+ //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
+ if(!$error) { 
+ $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+ $result = $statement->execute(array('email' => $email));
+ $user = $statement->fetch();
+ 
+ if($user !== false) {
+ echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+ $error = true;
+ } 
+ }
+ 
+ //Keine Fehler, wir können den Nutzer registrieren
+ if(!$error) { 
+ $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+ 
+ $statement = $pdo->prepare("INSERT INTO users (vorname, nachname, email, passwort) VALUES (:vorname, :nachname, :email, :passwort)");
+ $result = $statement->execute(array('vorname' => $vorname, 'nachname' => $nachname, 'email' => $email, 'passwort' => $passwort_hash));
+ 
+ if($result) { 
+ echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+ $showFormular = false;
+ } else {
+ echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+ }
+ } 
+}
+ 
+if($showFormular) {
+?>		
+<form action="?register=1" method="post">
+Vorname:<br>
+<input type="text" size="40" maxlength="250" name="vorname"><br><br>
+
+Nachname:<br>
+<input type="text" size="40" maxlength="250" name="nachname"><br><br>
+
+E-Mail:<br>
+<input type="email" size="40" maxlength="250" name="email"><br><br>
+ 
+Dein Passwort:<br>
+<input type="password" size="40"  maxlength="250" name="passwort"><br>
+ 
+Passwort wiederholen:<br>
+<input type="password" size="40" maxlength="250" name="passwort2"><br><br>
+ 
+<input type="submit" value="registrieren">
+</form>
+ 
+<?php
+} //Ende von if($showFormular)
+?>
+ 
+	<p><br/>Zurück zum <a href="login.php">Login</a></p>
+	
+	
+</body>
+</html>
