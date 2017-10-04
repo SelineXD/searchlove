1a2241414e830aca383a49dba91da024a88ef4ee
Naty2 <31928652+Naty2@users.noreply.github.com>
2017-10-04 16:36:19 +0200

    Natascha
    Profil erstellt

---
+++ profil.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+   <head>
+        <title>searchlove</title>
+		<link rel="stylesheet" href="style.css">
+    </head>
+	<body>
+	
+	<div id="header">
+	<div id="logo"><img src="logo.png" width="200px">
+		<br/><strong>das Datingportal</strong>
+	</div>
+	<div id="navi">
+	<div><ul><li><a href="login.php">Home</a></li>
+	<li><a href="logout.php">Logout</a></li></ul>
+	</div></div></div>
+	<div id="content">
+	
+	
+	<div id="profilbild">
+		<div id= "pb">
+		<img src="pb.jpg" width="200px" height="200px">
+		</div>
+	</div>
+	
+	<div id="angaben">
+	<form action="?register=1" method="post">
+	Vorname:<br>
+		<input type="text" size="40" maxlength="250" name="vorname">
+
+	Nachname:
+		<input type="text" size="40" maxlength="250" name="nachname"><br><br>
+
+	Jahrgang:<br>
+		<input type="text" size="40" maxlength="250" name="jahrgang"><br><br>
+
+	Geschlecht:<br>
+		<input type="text" size="40" maxlength="250" name="geschlecht"><br><br>
+
+	Interessiert an:<br>
+		<input type="text" size="40" maxlength="250" name="type"><br><br> 
+
+	Auf der Suche nach:<br>
+		<input type="text" size="40" maxlength="250" name="suche"><br><br>
+
+	Hobbys:<br>
+		<input type="text" size="40" maxlength="250" name="hobbys"><br><br>
+
+	Allgemeine Information:<br>
+		<input type="text" size="40" maxlength="250" name="info"><br><br>
+	</br></br>
+
+	</form> 
+	</div>
+	
+	
+	
+	<!--Bildergallery!-->
+	<h1>Beispiel Bildergalerie</h1>
+
+	<a href="bilder/tuebingen-1-gr.jpg" rel="lightbox[stadt]" title="Tübingen 1">
+		<img src="bildergalerie/tuebingen-1-kl.jpg" width="200" height="133">
+	</a>
+	<a href="bildergalerie/tuebingen-2-gr.jpg" rel="lightbox[stadt]" title="Tübingen 2">
+		<img src="bildergalerie/tuebingen-2-kl.jpg" width="200" height="133">
+	</a>
+	<a href="bildergalerie/tuebingen-3-gr.jpg" rel="lightbox[stadt]" title="Tübingen 3">
+		<img src="bildergalerie/tuebingen-3-kl.jpg" width="200" height="133">
+	</a>
+	<a href="bildergalerie/tuebingen-4-gr.jpg" rel="lightbox[stadt]" title="Tübingen 4">
+		<img src="bildergalerie/tuebingen-4-kl.jpg" width="133" height="200">
+	</a>
+
+	<script src="js/lightbox-plus-jquery.min.js"></script>
+	
+	https://www.html-seminar.de/bildergalerie-mit-lightbox.htm
+	
+	
+	<!--         APP  start        -->
+	http://cssapps.de/foto-gal/fgal_css3_click_perm02.html#t07
+
+<div id="container">
+<div id="window">
+<div id="all">
+    <div><img class="ls" src="../fotos/landschaft/land60.jpg" alt="picture"></div>
+    <div><img class="ls" src="../fotos/tiere/animal28.jpg" alt="picture"></div>
+    <div><img class="pt" src="../fotos/rummel/kairbr059.jpg" alt="picture"></div>
+    <div><img class="pt" src="../fotos/rummel/kirmes103.jpg" alt="picture"></div>
+    <div><img class="pt" src="../fotos/landschaft/land5.jpg" alt="picture"></div>
+    <div><img class="pt" src="../fotos/rummel/feuerwk0017.jpg" alt="picture"></div>
+    <div><img class="pt" src="../fotos/rummel/feuerwk0022.jpg" alt="picture"></div>
+    <div><img class="pt" src="../fotos/rummel/feuerwk0024.jpg" alt="picture"></div>
+    <div><img class="pt" src="../fotos/diverse/mond04.jpg" alt="picture"></div>
+    <div><img class="ls" src="../fotos/diverse/nacht14.jpg" alt="picture"></div>
+</div>
+</div>
+<ul>
+    <li><a href="#t01"><img src="../fotos/landschaft/land60.jpg" alt="picture"></a></li>
+    <li><a href="#t02"><img src="../fotos/tiere/animal28.jpg" alt="picture"></a></li>
+    <li><a href="#t03"><img src="../fotos/rummel/kairbr059.jpg" alt="picture"></a></li>
+    <li><a href="#t04"><img src="../fotos/rummel/kirmes103.jpg" alt="picture"></a></li>
+    <li><a href="#t05"><img src="../fotos/landschaft/land5.jpg" alt="picture"></a></li>
+    <li><a href="#t06"><img src="../fotos/rummel/feuerwk0017.jpg" alt="picture"></a></li>
+    <li><a href="#t07"><img src="../fotos/rummel/feuerwk0022.jpg" alt="picture"></a></li>
+    <li><a href="#t08"><img src="../fotos/rummel/feuerwk0024.jpg" alt="picture"></a></li>
+    <li><a href="#t09"><img src="../fotos/diverse/mond04.jpg" alt="picture"></a></li>
+    <li><a href="#t10"><img src="../fotos/diverse/nacht14.jpg" alt="picture"></a></li>
+</ul>
+</div>
+
+<!--         APP end           -->
+	
+	
+	
+
+	</div>
+
+	</body>
+</html>
