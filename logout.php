d01d7f94e1491287b4f4a6a546afc1c390e5b8a4
Naty2 <31928652+Naty2@users.noreply.github.com>
2017-10-13 16:19:50 +0200

    Natascha
    Share-Buttons hinzugefügt
    - Facebook
    - Twitter

--- logout.php
+++ logout.php
@@ -4,7 +4,17 @@
         <title>searchlove</title>
 		<link rel="stylesheet" href="style.css">
     </head>
-	<body>
+	<div id="share-buttons">
+	    		<!-- Facebook -->
+	   		 <a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank">
+	        		<img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
+	   		 </a>
+	   		<!-- Twitter -->
+	    		<a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
+	        		<img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
+	    		</a>
+		</div>
+<body>
 	
 	<div id="header">
 	<img src="logo.png" width="200px">
