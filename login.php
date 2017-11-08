835c9557ba417a2e056095461a6f6a643127b51d
Naty2 <31928652+Naty2@users.noreply.github.com>
2017-11-08 16:08:35 +0100

    Natascha
    Abstand zur Eingabe Passwort und zum Button Einloggen

--- login.php
+++ login.php
@@ -56,7 +56,7 @@ E-Mail:<br>
 <input type="email" size="40" maxlength="250" name="email"><br><br>
  
 Dein Passwort:<br>
-<input type="password" size="40"  maxlength="250" name="passwort"><br>
+	<input type="password" size="40"  maxlength="250" name="passwort"><br></br>
  
 <input type="submit" value="einloggen">
 </form> 
